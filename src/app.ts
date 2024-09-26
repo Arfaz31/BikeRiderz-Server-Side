7ece699dc69eebf200ef7fa10404d3a8d9045c3a
Arfaz31 <arfazahamed31@gmail.com>
2024-09-26 22:59:10 +0600

    new final commit

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,12 @@ import router from './app/routes';
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({
+    origin: 'https://bike-riderz-client-side.vercel.app',
+    credentials: true,
+  }),
+);
 app.use('/api', router);
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Bike Rental service');
