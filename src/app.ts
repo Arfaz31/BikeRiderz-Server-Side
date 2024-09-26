5dfd0ea5523c17e48d759f547447e3900daa9084
Arfaz31 <arfazahamed31@gmail.com>
2024-09-27 01:06:05 +0600

    app.ts file change

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'https://bike-riderz-client-side.vercel.app',
+    origin: 'http://localhost:5173',
     credentials: true,
   }),
 );
