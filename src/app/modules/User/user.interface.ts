27f0c5d69cd92be813d8904124612e4544b8ddda
Arfaz31 <arfazahamed31@gmail.com>
2025-04-09 22:15:33 +0600

    implement multer with cloudinary storage

--- src/app/modules/User/user.interface.ts
+++ src/app/modules/User/user.interface.ts
@@ -1,3 +1,4 @@
+import { Model } from 'mongoose';
 import { USER_Role } from './user.constants';
 
 export type Tuser = {
@@ -5,8 +6,19 @@ export type Tuser = {
   email: string;
   password: string;
   phone: string;
-  address: string;
+  address?: string;
+  nidCardNo: string;
+  profileImg?: string;
   role: keyof typeof USER_Role;
+  isDeleted?: boolean;
+  passwordChangedAt?: Date;
 };
 
 export type TUserRole = keyof typeof USER_Role;
+
+export interface UserModel extends Model<Tuser> {
+  isJWTIssuedBeforePasswordChanged(
+    passwordChangedTimestamp: Date,
+    jwtIssuedTimestamp: number,
+  ): boolean;
+}
